af4f809c6c5029793837c86de8163ad2eb06e0fc
niklang <nikolang@Nikos-MacBook-Pro.local>
2014-08-13 12:26:24 +0300

    Major fixes, new buttons.
    For the future: on comment-view, “post number” should be name of the
    post, not just number.

--- fw_plugin/activity.php
+++ fw_plugin/activity.php
@@ -3,5 +3,6 @@
           <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
           <script src="wp-content/plugins/fw_plugin/activityHandler.js"></script>
           <section id="activityWrapper"></section>
-          <button id="activityReloadButton" onclick="loadComments(); 											loadPosts()">Reload</button>';
+          <button id="PostsReloadButton" onclick="loadPosts()" style="width:95%;">Recent Posts</button>
+          <button id="CommentsReloadButton" onclick="loadComments()" style="width:95%;">Recent Comments</button>';
 ?>
--- fw_plugin/activityHandler.js
+++ fw_plugin/activityHandler.js
@@ -1,24 +1,49 @@
 $('document').ready(function(){
-    $('#activityReloadButton').on('click', function() {
-        $(this).prop("disabled", false)
+
+	/** 
+     * Function to handle click on the posts reload button,
+     * loads the recent posts and sets the button disabled
+     * until the request is done.
+     */
+    $('#PostsReloadButton').on('click', function() {
+        $(this).prop("disabled", true)
                .css("background-color", "red");
-        
     });
+    
+    /** 
+     * Function to handle click on the comments reload button,
+     * loads the recent comments and sets the button disabled
+     * until the request is done.
+     */
+    $('#CommentsReloadButton').on('click', function() {
+        $(this).prop("disabled", true)
+               .css("background-color", "red");
+    });
+    
+    /**
+     * Loads recent posts to show first. 
+     */
+    loadPosts();
 });
 
-
+/**
+ * Get posts from json.
+ */
 function loadPosts() {
 	httpGetPosts("/wp-json/posts");
 }
 
+/**
+ * Get comments from json.
+ */
 function loadComments() {
     httpGetComments("/wp-json/posts/0/comments");
 }
 
-function httpGetComments(theUrl) {
-/*
-* function httpGetPosts(theUrl) { ....
-*/
+/**
+ * Loads posts.
+ */
+function httpGetPosts(theUrl) {
     postsFetched = false;
     $('#activityWrapper').html('Reloading Activity...');
     fadeInResultText();
@@ -30,9 +55,67 @@ function httpGetComments(theUrl) {
         resumeText();
         var json = JSON.parse(JSON.stringify(data));
         transformToTable(json);
-        $('#activityReloadButton').prop("disabled", false)
+        $('#PostsReloadButton').prop("disabled", false)
                                   .css("background-color", "green");
     });
+}
+
+/**
+ * Loads comments.
+ */
+function httpGetComments(theUrl) {
+    postsFetched = false;
+    $('#activityWrapper').html('Reloading Activity...');
+    fadeInResultText();
+    
+    var jqxhr = $.ajax({
+        url: theUrl
+    }, 'json').done(function(data, text) {
+        postsFetched = true;
+        resumeText();
+        var json = JSON.parse(JSON.stringify(data));
+        transformComsToTable(json);
+        $('#CommentsReloadButton').prop("disabled", false)
+                                  .css("background-color", "green");
+    });
+}
+
+/**
+ * Transforms json-data from posts to table and prints the relevant info.
+ */
+function transformToTable(json) {
+    var elem = document.getElementById("activityWrapper");
+    elem.innerHTML = "<dl>";
+    
+    for (var i = 0; i < 5; i++){
+            elem.innerHTML += "<dt>"
+                           +  json[i]['title']
+						   +  "</dt>"
+						   +  "<dd>"
+                           +  json[i]['content']
+                           +  "</dd>";
+    }
+    elem.innerHTML += "</dl>";
+}
+
+/**
+ * Transforms json-data from comments to table and prints the relevant info.
+ */
+function transformComsToTable(json) {
+    var elem = document.getElementById("activityWrapper");
+    elem.innerHTML = "<dl>";
+    
+    for (var i = 0; i < 5; i++){
+            elem.innerHTML += "<dt>"
+            			   + "Comment on post "
+            			   +  json[i]['post']
+            			   + "<br>"
+						   +  "</dt>"
+						   +  "<dd>"
+                           +  json[i]['content']
+                           +  "</dd>";
+    }
+    elem.innerHTML += "</dl>";
 }
 
 function resumeText(){
@@ -41,11 +124,13 @@ function resumeText(){
 
 var postsFetched = false;
 
+/**
+ * Fades in and out the text indicating the state of activity-request.
+ */
 function fadeInResultText(){
     $( "#activityWrapper" ).animate({
         opacity: 0.5
     }, 1000, function() {
-        if (!postsFetched)
             fadeOutResultText();
     });
 }
@@ -58,21 +143,4 @@ function fadeOutResultText(){
         if (!postsFetched)
             fadeInResultText();
     });
-}
-
-function transformToTable(json) {
-    var elem = document.getElementById("activityWrapper");
-    elem.innerHTML = "<dl>";
-    
-    for (var i = 0; i < 5; i++){
-            elem.innerHTML += "<dt>"
-            			   +  json[i]['date']
-            			   + "<br>"
-                           +  json[i]['title']
-						   +  "</dt>"
-						   +  "<dd>"
-                           +  json[i]['content']
-                           +  "</dd>";
-    }
-    elem.innerHTML += "</dl>";
 }
